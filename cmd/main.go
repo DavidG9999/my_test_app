9b2340f75243c8656a9717834e994c2e11c1f6ec
DavidG9999 <bulbasik01@gmail.com>
2024-11-03 01:17:22 +0300

    Transfer of app and httpserver implementation to a separate repository

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,7 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/DavidG9999/my_test_app/interal/handler"
-	"github.com/DavidG9999/my_test_app/interal/repository"
-	"github.com/DavidG9999/my_test_app/interal/repository/postgres"
-	srv "github.com/DavidG9999/my_test_app/interal/server"
-	"github.com/DavidG9999/my_test_app/interal/service"
+	"github.com/DavidG9999/my_test_app/interal/app"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
@@ -21,7 +11,6 @@ import (
 // @title My Test App API
 // @version 1.0
 // @description API Server for Document Automation Service (Putlist)
-
 
 // @host localhost:8000
 // @BasePath /
@@ -37,51 +26,14 @@ func main() {
 		logrus.Fatalf("error initializiing configs: %s", err.Error())
 	}
 
-	if err := godotenv.Load("../.env"); err != nil {
+	if err := godotenv.Load(); err != nil {
 		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
-
-	db, err := postgres.NewPostgresDB(postgres.Config{
-		Host:     viper.GetString("db.host"),
-		Port:     viper.GetString("db.port"),
-		Username: viper.GetString("db.username"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DBName:   viper.GetString("db.dbname"),
-		SSLMode:  viper.GetString("db.sslmode"),
-	})
-	if err != nil {
-		logrus.Fatalf("failed to connect db: %s", err.Error())
-	}
-
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
-
-	srv := new(srv.Server)
-	go func() {
-		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-			log.Fatalf("error occured while running http server: %s", err.Error())
-		}
-	}()
-
-	logrus.Print("App started")
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
-	<-quit
-
-	logrus.Print("App shutting down")
-
-	if err := srv.Shutdown(context.Background()); err != nil {
-		logrus.Errorf("error occured on server shitting down: %s", err.Error())
-	}
-	if err := db.Close(); err != nil {
-		logrus.Errorf("error occured on db connection close: %s", err.Error())
-	}
+	app.RunApp()
 }
 
 func initConfig() error {
-	viper.AddConfigPath("../configs")
+	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
--- interal/app/httpserver/server.go
+++ interal/app/httpserver/server.go
@@ -1,4 +1,4 @@
-package interal
+package httpserver
 
 import (
 	"context"
